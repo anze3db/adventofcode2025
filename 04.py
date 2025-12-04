380dc17ba41554b03b787731c4e94f684345a165
Anže Pečar <anze@pecar.me>
2025-12-04 05:25:11 +0000

    Solve day 4

--- 04.py
+++ 04.py
@@ -4,18 +4,65 @@ Usage:
 
 uv run adventofcode run 04.py
 """
-inp = """your input"""
+
+inp = """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."""
 part1_asserts = [
-    (inp, None),
+    (inp, 13),
 ]
 part2_asserts = [
-    (inp, None),
+    (inp, 43),
 ]
+
+
+def num_adjecents(grid: set[tuple[int, int]], paper: tuple[int, int]) -> int:
+    count = 0
+    x, y = paper
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue
+            if (x + dx, y + dy) in grid:
+                count += 1
+    return count
 
 
 def part1(inp: str) -> str | int | None:
-    return None
+    count = 0
+    grid = set()
+    for i, line in enumerate(inp.splitlines()):
+        for j, char in enumerate(line):
+            if char == "@":
+                grid.add((i, j))
+    for paper in grid:
+        if num_adjecents(grid, paper) < 4:
+            count += 1
+
+    return count
 
 
 def part2(inp: str) -> str | int | None:
-    return None
+    count = 0
+    grid = set()
+    for i, line in enumerate(inp.splitlines()):
+        for j, char in enumerate(line):
+            if char == "@":
+                grid.add((i, j))
+    while True:
+        new_grid = set()
+        for paper in grid:
+            if num_adjecents(grid, paper) < 4:
+                count += 1
+            else:
+                new_grid.add(paper)
+        if new_grid == grid:
+            return count
+        grid = new_grid
